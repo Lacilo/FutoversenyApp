74002bd33de1583734e60bf594a3e21eab3fb340
lacos <szaszfail@kkszki.hu>
2026-01-13 20:49:46 +0100

    megjelenítés osztály létrehozása

---
+++ FutoversenyApp/Models/Display.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutoversenyApp.Models
+{
+    internal class Display
+    {
+        internal List<Futas> Futasok { get => Futasok; set => Futasok = value; }
+
+        public Display(List<Futas> futasok)
+        {
+            Futasok = futasok;
+        }
+
+        void UpdateFutasok(List<Futas> ujFutasok)
+        {
+            this.Futasok = ujFutasok;
+        }
+    }
+}
